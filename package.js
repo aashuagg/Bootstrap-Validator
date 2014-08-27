46433bab83ebb880edf1dbd4fe6d3c6dea4c64ba
Rishi Goomar <rishig92@gmail.com>
2014-08-26 21:41:38 -0500

    Updated for Meteor 0.9

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
-  summary: 'bootstrapValidator to validate forms and use bootstrap 3 to show errors'
+  summary: 'bootstrapValidator to validate forms and use bootstrap 3 to show errors',
+  version: '0.5.0',
+  githubUrl: 'https://github.com/rgoomar/Bootstrap-Validator'
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
+    api.versionsFrom("METEOR@0.9.0");
     api.use('jquery', 'client');
-    api.add_files('lib/js/bootstrapValidator.js', 'client');
-    api.add_files('lib/css/bootstrapValidator.css', 'client');
+    api.addFiles('lib/js/bootstrapValidator.js', 'client');
+    api.addFiles('lib/css/bootstrapValidator.css', 'client');
 });
